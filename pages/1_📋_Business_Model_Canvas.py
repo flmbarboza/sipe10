51389d95e450c1bf7076c94a0a3bf19938f9f2bd
Flavio Barboza <flmbarboza@gmail.com>
2026-07-12 13:09:11 -0300

    Update 1_📋_Business_Model_Canvas.py

--- pages/1_📋_Business_Model_Canvas.py
+++ pages/1_📋_Business_Model_Canvas.py
@@ -7,7 +7,7 @@ init_data()
 data = get_data()
 
 st.sidebar.title("🧭 Gestor Estratégico")
-sidebar_api_key_input()
+#sidebar_api_key_input()
 sidebar_data_controls()
 
 st.title("📋 Business Model Canvas")
