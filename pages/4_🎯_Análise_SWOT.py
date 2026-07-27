b9241f89f1738e9f7d5472922d488e4564f5f518
Flavio Barboza <flmbarboza@gmail.com>
2026-07-27 15:21:59 -0300

    Update 4_🎯_Análise_SWOT.py

--- pages/4_🎯_Análise_SWOT.py
+++ pages/4_🎯_Análise_SWOT.py
@@ -5,6 +5,11 @@ import re
 from utils.data_manager import init_data, get_data, sidebar_data_controls
 from utils.chat import render_chat
 from openai import OpenAI
+from utils.analytics import UXMonitor
+
+monitor = UXMonitor()
+page_name = "Análise SWOT"
+monitor.track_event('page_view', page_name)
 
 st.set_page_config(page_title="Análise SWOT", page_icon="🎯", layout="wide")
 init_data()
