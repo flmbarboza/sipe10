435a88118c0446d3a5ab21c42cf7feb20bd4ef1b
Flavio Barboza <flmbarboza@gmail.com>
2026-08-04 23:41:00 -0300

    Create enums.py

---
+++ analytics/enums.py
@@ -0,0 +1,48 @@
+from enum import Enum
+
+
+class EventType(str, Enum):
+    PAGE_OPEN = "page_open"
+    PAGE_CLOSE = "page_close"
+
+    MODULE_START = "module_start"
+    MODULE_FINISH = "module_finish"
+
+    BUTTON_CLICK = "button_click"
+
+    SAVE = "save"
+
+    AI_REQUEST = "ai_request"
+    AI_RESPONSE = "ai_response"
+
+    PDF_EXPORT = "pdf_export"
+
+    ERROR = "error"
+
+
+class Module(str, Enum):
+    HOME = "home"
+
+    CANVAS = "canvas"
+
+    PESTEL = "pestel"
+
+    PORTER = "porter"
+
+    SWOT = "swot"
+
+    PLANEJAMENTO = "planejamento"
+
+    FINANCEIRO = "financeiro"
+
+    PLANO_ACAO = "plano_acao"
+
+    MONITORAMENTO = "monitoramento"
+
+    REVIEW = "revisao"
+
+    PAINEL = "painel"
+
+    RELATORIO = "relatorio"
+
+    SYSTEM = "system"
