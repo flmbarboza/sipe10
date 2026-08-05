8a393e965b498cb89e48c7eaa692be149fd42ceb
Flavio Barboza <flmbarboza@gmail.com>
2026-08-05 09:25:11 -0300

    Update session.py

--- analytics/session.py
+++ analytics/session.py
@@ -1,6 +1,23 @@
 import streamlit as st
 import uuid
+import time
 
+
+def module_enter(module):
+
+    st.session_state["analytics_current_module"] = module
+
+    st.session_state["analytics_module_start"] = time.time()
+
+
+def module_duration():
+
+    inicio = st.session_state.get("analytics_module_start")
+
+    if inicio is None:
+        return 0
+
+    return int((time.time() - inicio) * 1000)
 
 def get_session():
 
