5d967fbf3866edd4b2ae478ee024643297a12bce
Flavio Barboza <flmbarboza@gmail.com>
2026-08-05 09:40:27 -0300

    Update tracker.py

--- analytics/tracker.py
+++ analytics/tracker.py
@@ -1,12 +1,11 @@
 from inspect import stack
-
 from utils.data_manager import get_data
-
 from .buffer import buffer
 from .models import Event, now, new_uuid
-from .session import get_session, get_operation
+from .session import get_session, get_operation, module_duration
+import time
 
-
+_last_event = None
 _last_timestamp = None
 
 
@@ -26,16 +25,21 @@ def track(
     action="",
     metadata=None
 ):
-
+    global _last_event
+    agora = time.time()
+    if _last_event is None:
+        duration = 0
+    else:
+        duration = int(
+            (agora - _last_event) * 1000
+        )
+    _last_event = agora
+    
     global _last_timestamp
-
     if metadata is None:
         metadata = {}
-
     data = get_data()
-
     empresa = data.get("empresa", {})
-
     e = Event(
         timestamp=now(),
         event_id=new_uuid(),
@@ -45,6 +49,8 @@ def track(
         module=module,
         event=event,
         action=action,
+        duration_ms=duration,
+        elapsed_module_ms=module_duration(),
         company_name=empresa.get("nome", ""),
         company_sector=empresa.get("setor", ""),
         metadata=metadata
