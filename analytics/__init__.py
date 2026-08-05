ce8f04d9ca2b15c1577811e3536ec3d203858697
Flavio Barboza <flmbarboza@gmail.com>
2026-08-05 00:14:46 -0300

    Update __init__.py

--- analytics/__init__.py
+++ analytics/__init__.py
@@ -1,22 +1,19 @@
 from .tracker import track
 from .operation import Operation
-
 from .enums import EventType
 from .enums import Module
-
 from .bus import event_bus
-
 from .console import console_handler
-
 from .buffer import buffer
-
 from .autoflush import autoflush
-
 from .sync import sync_engine
-
 from .migrations import migrate
-
 from .page_tracker import init_page
+from .session_manager import (
+    init_session,
+    update_activity,
+    check_timeout
+)
 
 migrate()
 
