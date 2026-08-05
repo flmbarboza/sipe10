28657cfe5c6a3a8febbaa86ec8bec67ab0cbf927
Flavio Barboza <flmbarboza@gmail.com>
2026-08-05 02:20:43 -0300

    Update 2_🌍_Análise_PESTEL.py

--- pages/2_🌍_Análise_PESTEL.py
+++ pages/2_🌍_Análise_PESTEL.py
@@ -7,6 +7,9 @@ from utils.chat import render_chat
 from openai import OpenAI
 from analytics import Module
 from analytics import module_started
+from analytics import module_completed
+from analytics import track
+from analytics import EventType
 
 st.set_page_config(page_title="Análise PESTEL", page_icon="🌍", layout="wide")
 init_data()
