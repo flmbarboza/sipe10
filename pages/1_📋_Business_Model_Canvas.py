a952dfe1c30891cedf384ea893b6213ed6531bda
Flavio Barboza <flmbarboza@gmail.com>
2026-08-05 11:24:14 -0300

    Update 1_📋_Business_Model_Canvas.py

--- pages/1_📋_Business_Model_Canvas.py
+++ pages/1_📋_Business_Model_Canvas.py
@@ -5,6 +5,7 @@ import uuid
 import re
 import streamlit.components.v1 as components    
 from openai import OpenAI
+from copy import deepcopy
 from utils.data_manager import init_data, get_data, sidebar_data_controls
 from utils.chat import render_chat
 from analytics import init_page
